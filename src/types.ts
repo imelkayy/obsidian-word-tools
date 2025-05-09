b04c415b53aef0e4e3fc7292b6b162b9b0fa78a8
elkayy <lkkcraft@gmail.com>
2025-05-09 12:17:01 -0700

    Define CountSettings type/interface

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface CountSettings {
+    /**
+     * Should text inside comments be counted towards the word count?
+     */
+    countComments: boolean,
+    /**
+     * Should the full text of links be counted, or just the alias (if provided)?
+     */
+    countFullLink: boolean,
+}
