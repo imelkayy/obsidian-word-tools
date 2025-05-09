d7d367250c9a9095aba0eb9fc4d00511a173a598
elkayy <lkkcraft@gmail.com>
2025-05-09 12:19:19 -0700

    Create parseContentBySettings()

--- src/wordCounter.ts
+++ src/wordCounter.ts
@@ -1,16 +1,25 @@
+import { CountSettings } from "./types";
+
 const SPLIT_REGEX = /(?<!-)\b\w+('\w+)?\b|\b\d+(,\d+)*\b/g 
 const COMMENT_REGEX = /\%\%.*\%\%/g
 const LINK_REGEX = /\[\[.*?\|(.*)\]\]/
 
-export default function getWordCount(content: string): number {
+export default function getWordCount(content: string, settings: CountSettings): number {
     content.replace("_", " ");
-    content = replaceAliasedLinks(content);
-    content = replaceComments(content);
-
+    content = parseContentBySettings(content, settings);
+    
     const matching = [...content.replace(/_/g, " ").matchAll(SPLIT_REGEX)]
     const count = matching.length
 
     return count;
+}
+function parseContentBySettings(content: string, settings: CountSettings): string {
+    if(!settings.countComments)
+        content = replaceComments(content);
+    if(!settings.countFullLink)
+        content = replaceAliasedLinks(content);
+
+    return content;
 }
 
 function replaceAliasedLinks(contents: string): string {   
