61ba07293746dbb9b971fad558b2fbf50ea4d66d
elkayy <lkkcraft@gmail.com>
2025-05-09 09:56:14 -0700

    Create function to replace aliased links with their alias

--- src/wordCounter.ts
+++ src/wordCounter.ts
@@ -1,3 +1,16 @@
+
+function replaceAliasedLinks(contents: string): string {
+    const LINK_REGEX = /\[\[.*?\|(.*)\]\]/
+   
+    let match;
+    while((match = LINK_REGEX.exec(contents)) != null) {
+        // Replace all occurances of the aliased link with its alias
+        contents = contents.replace(LINK_REGEX, match[1]);
+    }
+
+    return contents;
+}
+
 function replaceComments(contents: string): string {
     const COMMENT_REGEX = /\%\%.*\%\%/g
     return contents.replace(COMMENT_REGEX, "");
