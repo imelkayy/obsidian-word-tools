e14964ae6ca1844f686796113613dbf27716703e
elkayy <lkkcraft@gmail.com>
2025-05-10 20:00:52 -0700

    Switch to extracted doc & daily count helpers

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import { totalWordsToday, stripWordHistory } from './lib/wordTracker';
 import { today } from './lib/today';
 import getWordAndCharCounts from './lib/wordCounter';
 import { DEFAULT_SETTINGS, WordToolSettings, WordToolsSettingTab } from './settings';
+import { WordCountHelper } from './docCountHelper';
+import { DailyCountHelper } from './dailyCountHelper';
 
 export default class WordToolsPlugin extends Plugin {
 	settings: WordToolSettings;
-	dailyCountBarEl: HTMLElement;
-	docCurrentWordsBarEl: HTMLElement;
-	docCurrentCharsBarEl: HTMLElement;
 	globalWordCountEl: HTMLElement;
 	todayCount: number;
-	updateCount: Debouncer<[count: number], void>;
+
 	debouncedSave: Debouncer<[], void>;
-	debouncedGlobalUpdate: Debouncer<[], void>;
+
 	globalCountCache: {
 		[filepath: string]: {
 			cacheTime: number,
@@ -21,32 +20,26 @@ export default class WordToolsPlugin extends Plugin {
 			cc: number
 		}
 	} = {};
-	hasSel: boolean;
+
+	docCount: WordCountHelper;
+	dailyCount: DailyCountHelper;
 
 	PREFIX = "Word Tools";
 
 	async onload() {
-		// Create status bar elements
-		this.docCurrentWordsBarEl = this.addStatusBarItem();
-		this.docCurrentCharsBarEl = this.addStatusBarItem();
-		this.dailyCountBarEl = this.addStatusBarItem();
-
 		await this.loadSettings();
 
-		// Create debouncers for updating.
-		this.updateCount = debounce(this.handleCountUpdate, this.settings.displayUpdateDelay, false);
-		this.debouncedSave = debounce(this.handleDebouncedSave, this.settings.saveDelay, true);
-		this.debouncedGlobalUpdate = debounce(this.updateGlobalWordCount, this.settings.globalUpdateDelay, false);
+		this.docCount = new WordCountHelper(this);
+		this.dailyCount = new DailyCountHelper(this);
 
-		this.registerInterval(
-			window.setInterval(() => this.updateCurrentDocCounts(), 250)
-		)
+		// Create debouncers for updating.
+		this.debouncedSave = debounce(this.handleDebouncedSave, this.settings.saveDelay, true);
 
 		// Initialize today's word count if it doesn't exist
 		this.initDay(today());
 
 		// Update the current stored word count to today's saved value
-		this.updateCount(this.settings.history[today()].total);
+		this.dailyCount.updateTodayCount(this.settings.history[today()].total);
 		
 		// Add settigns tab
 		this.addSettingTab(new WordToolsSettingTab(this.app, this));
@@ -89,7 +82,7 @@ export default class WordToolsPlugin extends Plugin {
 	}
 
 	async saveSettings() {
-		this.updateStatusBarCount();
+		this.dailyCount.updateTodayCount(this.settings.history[today()]!.total);
 		await this.saveData(this.settings);
 	}
 
@@ -113,33 +106,6 @@ export default class WordToolsPlugin extends Plugin {
 			this.debouncedSave();
 		}
 		this.settings.history[TODAY].goal = this.settings.dailyWordGoal;
-	}
-
-	async updateCurrentDocCounts(words?: number, chars?: number) {
-		const sel = this.app.workspace.activeEditor?.editor?.getSelection();
-
-		if(sel) {
-			this.hasSel = true;
-			const count = getWordAndCharCounts(sel, this.settings.countSettings);
-			words = count.wc;
-			chars = count.cc;
-		} else if (this.hasSel && !words && !chars && this.app.workspace.activeEditor?.file) {
-			this.hasSel = false;
-			const read = await this.app.vault.cachedRead(this.app.workspace.activeEditor?.file!);
-			const count = getWordAndCharCounts(read, this.settings.countSettings);
-			words = count.wc;
-			chars = count.cc;
-		}
-
-		if(words)
-			this.docCurrentWordsBarEl.setText(`${words.toLocaleString()} words`)
-		if(chars)
-			this.docCurrentCharsBarEl.setText(`${chars.toLocaleString()} characters`);
-	}
-
-	updateStatusBarCount() {
-		const text = `${this.todayCount}${this.settings.showGoal ? `/${this.settings.dailyWordGoal}` : ""} words today `
-		this.dailyCountBarEl.setText(text);
 	}
 
 	async updateGlobalCountCacheForFile(file: TFile) {
@@ -168,22 +134,12 @@ export default class WordToolsPlugin extends Plugin {
 		}
 	}
 
-	handleCountUpdate(count: number) {
-		this.todayCount = count;
-
-		if(this.dailyCountBarEl)
-			this.updateStatusBarCount();
-		
-		if(this.debouncedSave)
-			this.debouncedSave();
-	}
-
 	onQuickPreview(file: TFile, contents: string) {
 		const PATH = file.path;
 		const COUNTS = getWordAndCharCounts(contents, this.settings.countSettings);
 		const TODAY = today();
 		
-		this.updateCurrentDocCounts(COUNTS.wc, COUNTS.cc)
+		this.docCount.updateCurrentDocCounts(COUNTS.wc, COUNTS.cc)
 
 		this.initFileHistory(PATH, COUNTS.wc, TODAY);
 		if(!this.settings.history[TODAY].files)
@@ -193,8 +149,8 @@ export default class WordToolsPlugin extends Plugin {
 		const TOTAL = totalWordsToday(this.settings.history);
 		this.settings.history[TODAY].total = TOTAL;
 
-		this.updateCount(TOTAL);
-		this.debouncedGlobalUpdate();
+		this.dailyCount.updateTodayCount(TOTAL);
+		this.docCount.updateCurrentDocCounts();
 	}
 
 	onFileRenamed(file: TFile, oldPath: string) {
@@ -218,7 +174,7 @@ export default class WordToolsPlugin extends Plugin {
 		const READ = await this.app.vault.cachedRead(file);
 		const DOC_COUNTS = getWordAndCharCounts(READ, this.settings.countSettings);
 
-		this.updateCurrentDocCounts(DOC_COUNTS.wc, DOC_COUNTS.cc)
+		this.docCount.updateCurrentDocCounts(DOC_COUNTS.wc, DOC_COUNTS.cc)
 
 		if(!this.settings.history[TODAY].files)
 			return;
